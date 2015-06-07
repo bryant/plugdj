14521fd576ec74ae6aa445fdf4319f3f306bc70f
bryant <bryant@defrag.in>
2015-06-07 07:09:22 -0400

    be nice. only login once.

--- plugdj/tests/__init__.py
+++ plugdj/tests/__init__.py
@@ -4,9 +4,18 @@ from . import _creds as creds
 def get_plug(listener=None):
     return PlugDJ(creds.email, creds.password, listener)
 
-def test_login():
-    p = get_plug()
+def is_ok(json):
+    return json["status"] == "ok"
 
-#def test_join_room():
-#    p = get_plug()
-#    p.join_room(creds.room)
+def test_join_room():
+    assert is_ok(p.join_room(creds.room))
+
+def test_chat():
+    assert is_ok(p.join_room(creds.room))
+    p.send_chat("hello world!")
+
+def test_really_long_chat():
+    assert is_ok(p.join_room(creds.room))
+    p.send_chat("hello world!" * 256)
+
+plug = get_plug()
