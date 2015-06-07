80814e7ec17e5671f803faba7641271af0869f0c
bryant <bryant@defrag.in>
2015-06-07 05:57:03 -0400

    add some tests; gitignore creds

---
+++ plugdj/tests/__init__.py
@@ -0,0 +1,12 @@
+from plugdj import PlugDJ
+from . import _creds as creds
+
+def get_plug(listener=None):
+    return PlugDJ(creds.email, creds.password, listener)
+
+def test_login():
+    p = get_plug()
+
+def test_join_room():
+    p = get_plug()
+    p.join_room(creds.room)
